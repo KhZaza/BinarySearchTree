71ba34e6eaa2d16763097f858d110a00bbe03641
Khaled Zaza <khaledzaza.cs@gmail.com>
2022-10-27 13:08:41 -0700

    Added the Node class

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,2 +1,30 @@
 public class BinaryTree {
+
+    public static void main(String[] args){
+
+    }
+
+}
+
+
+
+//class node to setup the nodes and add the keys to them
+class Node{
+    int key;
+    String name;
+
+
+    Node leftChild;
+    Node rightChild;
+
+    Node(int key, String name){
+        this.key = key;
+        this.name = name;
+    }
+
+    public String toString(){
+        return name + " has a key " + key;
+    }
+
+
 }
