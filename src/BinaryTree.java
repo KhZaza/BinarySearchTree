78aae175cfd73eb916eedf2ac8e6894684fb919a
Khaled Zaza <khaledzaza.cs@gmail.com>
2022-10-27 16:20:46 -0700

    Added the remove Node

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -65,7 +65,7 @@ public class BinaryTree {
 
         }
     }
-
+//search for a node
     public Node findNode(int key){
         Node focusNode  = root;
         while (focusNode.key != key){
@@ -84,6 +84,101 @@ public class BinaryTree {
     }
 
 
+    public boolean remove (int key){
+        Node focusNode = root;
+        //to start at the top of the binary tree
+        Node parent = root;
+
+        boolean isItALeftChild = true;
+
+        while (focusNode.key != key){
+
+            parent = focusNode;
+
+            //search to the left
+            if(key < focusNode.key){
+                isItALeftChild = true;
+                focusNode = focusNode.leftChild;
+
+            }
+            //search to the right
+            else {
+                isItALeftChild = false;
+                focusNode = focusNode.rightChild;
+            }
+
+            if(focusNode == null)
+                return false;
+
+        }
+        //Delete if they are in a leaf format or they don't have any children
+        if(focusNode.leftChild == null && focusNode.rightChild == null){
+
+            if(focusNode == root){
+                root = null;
+            }
+
+            else if(isItALeftChild){
+                parent.leftChild = null;
+            }
+            else{
+                parent.rightChild = null;
+            }
+        }
+        //Delete if there is no right child
+        else if( focusNode.rightChild == null){
+            if(focusNode == root)
+                root = focusNode.leftChild;
+            else if (isItALeftChild)
+                parent.leftChild = focusNode.leftChild;
+            else
+                parent.rightChild = focusNode.leftChild;
+        }
+        //if there is no left child
+        else if( focusNode.leftChild == null){
+            if(focusNode == root)
+                root = focusNode.rightChild;
+            else if (isItALeftChild)
+                parent.leftChild = focusNode.rightChild;
+            else
+                parent.rightChild = focusNode.leftChild;
+        }
+        else{
+            Node replacement = getReplacementNode(focusNode);
+            if (focusNode == root){
+                root = replacement;
+            }
+            else if (isItALeftChild)
+                parent.leftChild = replacement;
+            else
+                parent.rightChild = replacement;
+
+            replacement.leftChild = focusNode.leftChild;
+        }
+        return true;
+
+    }
+
+    public Node getReplacementNode(Node replaceNode){
+        Node replacementParent = replaceNode;
+        Node replacement =replaceNode;
+        Node focusNode = replaceNode.rightChild;
+
+        while(focusNode!= null){
+            replacementParent = replacement;
+            replacement = focusNode;
+            focusNode = focusNode.leftChild;
+        }
+        if (replacement != replaceNode.rightChild){
+            replacementParent.leftChild = replacement.rightChild;
+            replacement.rightChild = replaceNode.rightChild;
+        }
+        return replacement;
+    }
+
+
+
+
     public static void main(String[] args){
         BinaryTree theTree = new BinaryTree();
         theTree.addNode(50,"Boss");
@@ -93,10 +188,16 @@ public class BinaryTree {
         theTree.addNode(75,"CTO");
         theTree.addNode(85,"Sales");
 
-        theTree.postOrderTraverseTree(theTree.root);
+        System.out.println("Remove Key 25");
+        theTree.remove(25);
 
-        System.out.println("Search for 30");
-        System.out.println(theTree.findNode(30));
+
+        theTree.preOrderTraverseTree(theTree.root);
+
+//        System.out.println("Search for 30");
+//        System.out.println(theTree.findNode(30));
+//
+
 
 
     }
